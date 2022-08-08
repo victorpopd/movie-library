d14c9f1989e79cae3efb3f79a745b775f494e2ca
victorpopd <victor.pop@yopeso.com>
2022-08-08 09:28:15 +0300

    Commit

--- src/Main Pages/MoviePage/MoviePage.js
+++ src/Main Pages/MoviePage/MoviePage.js
@@ -3,11 +3,13 @@ import "./MoviePage.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MoviePage() {
   const pathId = useParams();
   // console.log(pathId);
   const [moviePage, setmoviePage] = useState(null);
+  const [similarMovie, setsimilarMovie] = useState([]);
 
   useEffect(() => {
     axios
@@ -17,7 +19,23 @@ function MoviePage() {
       .then(function (response) {
         // console.log(response);
         if (response.data) {
-        setmoviePage(response.data);
+          setmoviePage(response.data);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${pathId.id}/similar?api_key=388d899bacdfb56d15ed5ef6cac36a37&language=en-US&page=1`
+      )
+      .then((response) => {
+        // console.log(response);
+        if (response.data && response.data.results) {
+          setsimilarMovie(response.data.results);
         }
       })
       .catch((err) => {
@@ -42,9 +60,20 @@ function MoviePage() {
 
             <div className="bottom-details">
               <p>Release date: {moviePage.release_date}</p>
-              <p>Rating: {moviePage.vote_average}</p>
+              <p>Rating: {moviePage.vote_average.toFixed(1)}</p>
             </div>
           </div>
+        </div>
+        <div  className="similar-movies-container">
+          {similarMovie.slice(0, 6).map((movie) => (
+            <Link key={movie.id} to={`${movie.id}`}>
+              <div>
+                <img
+                  src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                ></img>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     );
